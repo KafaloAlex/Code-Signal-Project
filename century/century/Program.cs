029bb14782d396ab5101a1cc8b20e0ed2f5d6813
Coulibaly Kafalo Alex <ckalexis20@gmail.com>
2020-03-01 02:40:24 +0000

    Century Programs

---
+++ century/century/Program.cs
@@ -0,0 +1,36 @@
+﻿//Ce programme demande à l'utilisateur d'entrer une date comprise entre 1 et 2005 puis à quel siècle correspond cette année
+using System;
+
+namespace century
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entrez une année comprise entre 1 et 2005 :");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                if (year < 1 && year > 2005)
+                    Console.WriteLine("Vous devez entrer une date comprise entre 1 et 2005");
+                else if (year >= 1 && year < 100)
+                    Console.WriteLine("Vous êtes au premier siècle");
+                else
+                {
+                    int century = centuryFromYear(year);
+                    Console.WriteLine("L'{0} correspond au {1} siècle", year, century);
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Entrez une année correcte svp!!");
+            }
+        }
+
+        static int centuryFromYear(int x)
+        {
+            return (int)(x / 100) + ((x % 100 == 0) ? 0 : 1);
+        }
+    }
+}
---
+++ palindrome/palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿//Un palindrome est un mot dont l'ordre reste le même de gauche à droite et inversement.
+//Le programme ramène *True* si le mot est palindrome ,et *False* dans le cas contraire.
+using System;
+using System.Linq;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entrez un mot");
+                string chaine = Console.ReadLine();
+
+                checkPalindrome(chaine);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Nous avons seulement besoin d'un mot");
+            }
+        }
+
+        static bool checkPalindrome( string word)
+        {
+            bool isPalindrome = false;
+            String chaineReverse = new string(word.ToCharArray().Reverse().ToArray());
+
+            if (word == chaineReverse)
+            {
+                isPalindrome = true;
+                Console.WriteLine(isPalindrome);
+            }  
+            else
+            {
+                isPalindrome = false;
+                Console.WriteLine(isPalindrome);
+            }
+           
+            return isPalindrome;
+        }
+    }
+}
